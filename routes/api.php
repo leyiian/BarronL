95f408dc6b6b35093945e040802b28721059644a
Orst547 <urielpes.ups@gmail.com>
2024-07-11 23:22:34 -0600

    ya esta en el midleware las apis

--- routes/api.php
+++ routes/api.php
@@ -8,28 +8,33 @@ use App\Http\Controllers\PacienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
-Route::post('login',[LoginController::class,'login']);
-Route::post('register',[LoginController::class,'registro']);
+    Route::post('cita/guardar', [CitasController::class, 'saveApi']);
+    Route::get('citas', [CitasController::class, 'listApi']);
 
-Route::get('especialidades', [EspecialidadController::class,'listApi']);
-Route::post('especialidad', [EspecialidadController::class,'getApi']);
-Route::post('especialidad/guardar', [EspecialidadController::class,'saveApi']);
-Route::post('especialidad/eliminar', [EspecialidadController::class,'deleteApi']);
+    Route::get('especialidades', [EspecialidadController::class, 'listApi']);
+    Route::post('especialidad', [EspecialidadController::class, 'getApi']);
+    Route::post('especialidad/guardar', [EspecialidadController::class, 'saveApi']);
+    Route::post('especialidad/eliminar', [EspecialidadController::class, 'deleteApi']);
 
-Route::get('doctores', [DoctorController::class,'listApi']);
-Route::post('doctor', [DoctorController::class,'getApi']);
-Route::post('doctores/guardar', [DoctorController::class,'saveApi']);
-Route::post('doctores/eliminar', [DoctorController::class,'deleteApi']);
+    Route::get('doctores', [DoctorController::class, 'listApi']);
+    Route::post('doctor', [DoctorController::class, 'getApi']);
+    Route::post('doctores/guardar', [DoctorController::class, 'saveApi']);
+    Route::post('doctores/eliminar', [DoctorController::class, 'deleteApi']);
 
 
-Route::get('pacientes', [PacienteController::class,'listApi']);
-Route::post('paciente', [PacienteController::class,'getApi']);
-Route::post('paciente/guardar', [PacienteController::class,'saveApi']);
-Route::post('pacientes/eliminar', [PacienteController::class,'listApi']);
+    Route::get('pacientes', [PacienteController::class, 'listApi']);
+    Route::post('paciente', [PacienteController::class, 'getApi']);
+    Route::post('paciente/guardar', [PacienteController::class, 'saveApi']);
+    Route::post('pacientes/eliminar', [PacienteController::class, 'listApi']);
+});
 
-Route::post('cita/guardar', [CitasController::class, 'saveApi']);
-Route::get('citas', [CitasController::class, 'listApi']);
+
+Route::post('login', [LoginController::class, 'login']);
+Route::post('register', [LoginController::class, 'registro']);
+
+
